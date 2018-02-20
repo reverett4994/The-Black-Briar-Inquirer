077a21170313fb2fff58cc03acef1a56bb2b920a
Ryan Everett <reverett4994@gmail.com>
2018-02-20 04:20:00 -0500

    init push to server

--- wp-config.php
+++ wp-config.php
@@ -79,6 +79,12 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
--- wp-content/themes/inquirer theme/header.php
+++ wp-content/themes/inquirer theme/header.php
@@ -3,11 +3,9 @@
   <head>
     <meta charset=" <?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link href="https://fonts.googleapis.com/css?family=Lato|Overpass+Mono|UnifrakturCook:700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-
     <title> <?php bloginfo('name')?></title>
     <?php wp_head();?>
   </head>
@@ -19,13 +17,14 @@
           <a href="/newspaper">The Black-Briar Inquirer</a>
         </div>
       <div class="sub_logo">
+        Daily(ish) News and Information
           <div class="left">
             <?php
             echo date("l, M d Y ");
             ?>
           </div>
 
-          Daily(ish) News and Information
+
           <div class="right">
             <a href="#">Valenwood Edition</a>
           </div>
